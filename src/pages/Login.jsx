3248d68d3244e5031584e277558a474e29e1c61e
Sreerag <sreeragmohan5@gmail.com>
2026-03-13 22:12:34 +0530

    Step 3: Fix AuthProvider component naming

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,17 @@
-function Login () {
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+function Login() {
+  const { login } = useContext(AuthContext);
+
   return (
     <div>
-      <h1>Login page</h1>
+      <h1>Login Page</h1>
+      <button onClick={() => login("user@test.com")}>
+        Login
+      </button>
     </div>
-  )
-
+  );
 }
-
 
 export default Login;
